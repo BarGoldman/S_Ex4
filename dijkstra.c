c773833b39faaecfa4d6f7e2a5f61e256fe5ee41
BarGoldman <Bar8770@gmail.com>
2022-01-08 18:51:09 +0200

    v

--- dijkstra.c
+++ dijkstra.c
@@ -2,42 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "dijkstra.h"
 
 
 // Utility function to find minimum distance vertex in mdist
 int minDistance(int mdist[], int vset[], int V)
 {
-    int minVal = INT_MAX;
-    static int minInd = -1;
-    for (int i = 0; i < V; i++)
-        if (vset[i] == 0 && mdist[i] < minVal)
+    int min_val = INT_MAX;
+    static int min_ind = -1;
+    for (int i = 0; i < V; i++){
+        if (vset[i] == 0 && mdist[i] < min_val)
         {
-            minVal = mdist[i];
-            minInd = i;
+            min_val = mdist[i];
+            min_ind = i;
         }
-    //print minInd
-    // printf("flag is %d, minIndx=%d \n",flag, minInd);
-    return minInd;
-}
-
-// Utility function to print distances
-void print(int dist[], int V)
-{
-    printf("\nVertex  Distance\n");
-    for (int i = 0; i < V; i++)
-    {
-        if (dist[i] != INT_MAX)
-            printf("%d\t%d\n", i, dist[i]);
-        else
-            printf("%d\tINF", i);
     }
+    return min_ind;
 }
 
-// The main function that finds the shortest path from given source
-// to all other vertices using Dijkstra's Algorithm.It doesn't work on negative
-// weights
 int Dijkstra(struct GraphDijkstra *graph, int src,int dest)
 {
     int V = graph->vertexNum;
@@ -62,14 +44,10 @@ int Dijkstra(struct GraphDijkstra *graph, int src,int dest)
 
         for (int v = 0; v < V; v++)
         {
-            if (!vset[v] && graph->edges[u][v] != INT_MAX &&
-                mdist[u] + graph->edges[u][v] < mdist[v])
+            if (!vset[v] && graph->edges[u][v] != INT_MAX && mdist[u] + graph->edges[u][v] < mdist[v])
                 mdist[v] = mdist[u] + graph->edges[u][v];
         }
-    }
-
-    // print(mdist, V);
-    
+    }   
     //ternary operator
     return mdist[dest] != INT_MAX ? mdist[dest] : -1;
 }
